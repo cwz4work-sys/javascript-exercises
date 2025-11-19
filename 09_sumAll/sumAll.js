dafaab948138757425e9c1904294c3238e9953e4
WZ <cwz4work@gmail.com>
2025-11-19 16:34:25 +0800

    Use for loop to sum from smaller to bigger integer

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    for (let i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] !== 'number' || arguments[i] <= 0 ||
+            Math.floor(arguments[i]) !== arguments[i]
+        ) {return 'ERROR'};
+    }
 
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+
+    let total = 0;
+
+    for (; num1 <= num2; num1++) {
+        total += num1;
+    }
+    return total;
 };
 
 // Do not edit below this line
