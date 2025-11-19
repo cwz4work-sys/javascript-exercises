efe122ed440f8506247bd9ed2bee16914c77e425
WZ <cwz4work@gmail.com>
2025-11-19 16:11:42 +0800

    Use for loop with filter to remove arguments from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        array = array.filter((num) => num !== arguments[i])
+    }
+    return array;
 };
 
 // Do not edit below this line
