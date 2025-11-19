f5083b73f314b0f810e22781cf433cc5c403691d
WZ <cwz4work@gmail.com>
2025-11-19 15:39:40 +0800

    Create repeatString function with for loop

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        let result = '';
+        for (; num > 0; num--) {
+            result += string
+        }
+        return result
+    }
 };
 
 // Do not edit below this line
