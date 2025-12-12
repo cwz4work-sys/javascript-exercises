567456e7e64166d0e212652bfe2683e7367c8275
WZ <cwz4work@gmail.com>
2025-12-12 21:32:42 +0800

    Complete 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(place) {
+    place = parseInt(place);
+    let prevNum = 1;
+    let currentNum = 1;
+    if (place < 0) return 'OOPS';
+    else if (place === 0) return 0;
+    else if (place === 1 || place === 2) return prevNum;
+    for (let i = place - 2; i > 0; i--) {
+        const storePrevNum = prevNum;
+        prevNum = currentNum;
+        currentNum += storePrevNum;
+    }
+    return currentNum;
 };
 
 // Do not edit below this line
