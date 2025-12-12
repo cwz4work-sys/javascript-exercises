7cf1008d7f247cc5d1457d339dd95e92c73abe58
WZ <cwz4work@gmail.com>
2025-12-12 20:44:47 +0800

    Complete 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -6,20 +6,25 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, expo) {
+	return base ** expo;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fac = 1;
+  if (num === 0 || num === 1) return fac;
+	for (let i = num; num > 1; num--) {
+    fac *= num;
+  }
+  return fac;
 };
 
 // Do not edit below this line
