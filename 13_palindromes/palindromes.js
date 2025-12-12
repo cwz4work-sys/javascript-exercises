912e17ccf50e2716cd33ab0ae3e9b8582c929182
WZ <cwz4work@gmail.com>
2025-12-12 21:10:44 +0800

    Complete 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/\W/g, '').toLowerCase();
+    for (let i = 0; i <= Math.floor(word.length / 2); i++) {
+        if (word[i] !== word[word.length - i - 1]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
